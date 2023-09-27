5e4028a78e309a68f50c7f1ea25c2ee9c1901a3f
jamesS0804 <sucaldito.james0804@gmail.com>
2023-09-28 01:06:22 +0800

    [FEAT & MAINT]: Added HTML structure to SignupPage; Added Typing to useState userType and setUserType

--- Client/src/Pages/SignupPage.tsx
+++ Client/src/Pages/SignupPage.tsx
@@ -1,7 +1,22 @@
-export default function SignupPage(){
+import Header from "@/Main Components/Header";
+import { Button } from "@/components/ui/button";
+import homeowner from "../assets/images/homeowner.png"
+import housemates from "../assets/images/housemates.png"
+
+interface SignupPageProps {
+    userType: string;
+}
+
+export default function SignupPage(props:SignupPageProps){
+    const { userType } = props
     return(
-        <div>
-            
+        <div className="h-screen flex flex-col">
+            <Header />
+            <div className="mt-4 flex flex-col p-4 items-center border-2 grow">
+                <h3 className={`font-bold text-xl ${userType === 'Homeowner' ? 'text-primary' : 'text-secondary'}`}>Sign Up as a {userType}</h3>
+                <img src={userType === 'Homeowner' ? homeowner : housemates}/>
+            </div>
+            <Button className={`page-action-button text-white border-none rounded-none ${userType === 'Homeowner' ? 'bg-primary' : 'bg-secondary'}`}>Sign Up</Button>
         </div>
     )
 }
