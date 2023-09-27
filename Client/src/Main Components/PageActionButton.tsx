9961269aece0f356e52113004f3c3e65e8c9b984
jamesS0804 <sucaldito.james0804@gmail.com>
2023-09-27 22:47:43 +0800

    [FEAT]: Finished HTML structure and CSS of GetStartedPage and Header Component

--- Client/src/Main Components/PageActionButton.tsx
+++ Client/src/Main Components/PageActionButton.tsx
@@ -2,6 +2,6 @@ import { Button } from '../components/ui/button'
 
 export default function PageActionButton(){
     return(
-        <Button className='bg-primary'>Button</Button>
+        <Button className='bg-primary w-full fixed left-0 bottom-0'>Button</Button>
     )
 }
--- Client/src/Pages/GetStartedPage.tsx
+++ Client/src/Pages/GetStartedPage.tsx
@@ -1,9 +1,22 @@
-import PageActionButton from "../Components/PageActionButton"
+import Header from "@/Main Components/Header"
+import { Button } from "@/components/ui/button"
+import housemates from "../assets/images/housemates.png"
+
 export default function GetStartedPage() {
     return(
-        <div className="border-2 h-full border-solid border-black">
-            <h1>GetStartedPage</h1>
-            <PageActionButton />
+        <div className="h-full border-2 border-red-500">
+            <div className="mb-[8rem]" />
+            <Header />
+            <div className="w-full mt-[12rem] p-4">
+                <div className="relative h-[6.56rem] w-full rounded-xl bg-[#E6BB76]">
+                    <img className="absolute top-[-100%] m-auto left-0 right-0 h-50 w-[50%]" src={housemates}/>
+                    <div className="absolute top-[-1.25rem] flex w-full">
+                        <p className="mr-auto text-sm font-bold">What can we</p>
+                        <p className="ml-auto text-sm font-bold">help you with?</p>
+                    </div>
+                </div>
+            </div>
+            <Button className='bg-primary font-black text-lg w-full fixed left-0 bottom-0 rounded-none'>GET STARTED</Button>
         </div>
     )
 }
