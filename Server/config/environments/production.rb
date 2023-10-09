7654de5f94bb773d411fb163f23244c5f197c693
jamesS0804 <sucaldito.james0804@gmail.com>
2023-10-09 23:29:34 +0800

    [MAINT]: Added smtp configuration to production.rb to enable render.com to send emails on my behalf.

--- Server/config/environments/production.rb
+++ Server/config/environments/production.rb
@@ -83,4 +83,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'https://housemates-backend.onrender.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            ENV['GMAIL_APP_USERNAME'],
+    password:             ENV['GMAIL_APP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 
+  }
 end
