000f528f6f284d2d34545297003cf4e30bb169ee
jamesS0804 <sucaldito.james0804@gmail.com>
2023-10-11 22:49:42 +0800

    [FEAT & RE-FACTOR]: In ProfilesController, added address to JSON rendered; Re-factored controller to keep things DRY

--- Server/app/controllers/api/v1/profiles_controller.rb
+++ Server/app/controllers/api/v1/profiles_controller.rb
@@ -5,12 +5,7 @@ class Api::V1::ProfilesController < ApplicationController
             profile = user.build_profile(profile_params.merge(user_id: user.id))
             address = profile.build_address(profile_params[:address_attributes])
             if profile.save && address.save
-                profile_data = ProfileSerializer.new(profile).serializable_hash[:data][:attributes]
-                address_data = AddressSerializer.new(address).serializable_hash[:data][:attributes]
-
-                combined_data = profile_data.merge(address: address_data)
-
-                render json: { data: combined_data }, status: :created
+                render_profile_json(profile, address, status)
             else
                 render json: profile.errors, status: :unprocessable_entity
             end
@@ -22,11 +17,22 @@ class Api::V1::ProfilesController < ApplicationController
     def show
         user = User.find(params[:id])
         profile = user.profile
-        serialized_data = ProfileSerializer.new(profile).serializable_hash[:data][:attributes]
-        render json: { data: serialized_data, status: :ok }
+        address = profile.address
+        render_profile_json(profile, address, status)
     end
 
     private
+
+    def serialize_data(profile, address)
+        profile_data = ProfileSerializer.new(profile).serializable_hash[:data][:attributes]
+        address_data = AddressSerializer.new(address).serializable_hash[:data][:attributes]
+        profile_data.merge(address: address_data)
+    end
+
+    def render_profile_json(profile, address, status)
+        serialized_data = serialize_data(profile, address)
+        render json: { data: serialized_data, status: status }
+    end
 
     def profile_params
         params.require(:profile).permit(:id, :name, :phone_number, :email, address_attributes: [:address_line_1, :barangay, :city, :province, :zip_code])
