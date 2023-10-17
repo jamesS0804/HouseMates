171d9aba11415173e21a6d3dd0b15a874bb56456
jamesS0804 <sucaldito.james0804@gmail.com>
2023-10-17 19:22:19 +0800

    [FIX]: Fixed type mismatch for getting total costs resulting to NaN

--- Client/src/utils/getTotalCost.ts
+++ Client/src/utils/getTotalCost.ts
@@ -1,12 +1,12 @@
 export default function getTotalCost(serviceDetails:any){
     const totalPrice =
-        serviceDetails.service.price +
-        serviceDetails.data['Home Type'].price +
-        serviceDetails.data['Room Type'].price +
-        serviceDetails.data['Bathroom Count'].price +
+        Number(serviceDetails.service.price) +
+        Number(serviceDetails.data['Home Type'].price) +
+        Number(serviceDetails.data['Room Type'].price) +
+        Number(serviceDetails.data['Bathroom Count'].price) +
         serviceDetails.data['Extra Service'].reduce((acc: number, service: { quantity?: number, price: number }) => {
             const quantity = service.quantity || 1;
-            return acc + service.price * quantity;
+            return Number(acc) + Number(service.price) * Number(quantity);
         }, 0);
     return totalPrice
 }
